35a50d3560206b38c745f32b60d3919c204b8bbf
anyung533 <860300966@qq.com>
2022-09-19 18:18:54 +0800

    no message

--- pull.sh
+++ pull.sh
@@ -1,15 +1,15 @@
 #cron 20 * * * * pull.sh
-ql repo https://github.com/ANYUNG533/jd.git "jd_|jx_|gua_|zy_|jd-task-|jddj_|ql_|ck_" "util|utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron|sillyGirl|dockerbot" "" "main"
+ql repo https://github.com/ANYUNG533/jd.git "jd_|jx_|gua_|zy_|jd-task-|jddj_|ql_|ck_" "util|utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron|sillyGirl|dockerbot" "" "JOJO"
 
 
 
-cp -rfv /ql/repo/ANYUNG533_jd/* /ql/scripts/
+cp -rfv /ql/repo/ANYUNG533_jd_JOJO/* /ql/scripts/
 
-cp -rfv /ql/repo/ANYUNG533_jd/* /ql/scripts/ANYUNG533_jd/
+cp -rfv /ql/repo/ANYUNG533_jd_JOJO/* /ql/scripts/ANYUNG533_jd_JOJO/
 
-cp -rfv /ql/repo/ANYUNG533_jd/pull.sh /ql/config/
+cp -rfv /ql/repo/ANYUNG533_jd_JOJO/pull.sh /ql/config/
 
-cp -rfv /ql/repo/ANYUNG533_jd/sendNotify.js /ql/deps/
+cp -rfv /ql/repo/ANYUNG533_jd_JOJO/sendNotify.js /ql/deps/
 
 
 
